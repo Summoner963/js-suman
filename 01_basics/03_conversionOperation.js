38f074672a7341c57791458514f546492d556aec
Summoner963 <theonesummonerzzz@gmail.com>
2025-08-10 10:40:43 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score1 = "33"
+let score ="33abc" // op is NaN
+// let score =null // op is 0
+// let score =undefined  // op is NaN
+// let score =true/false // op is 1/0
+
+console.log(typeof score)
+
+let convertedScore = Number(score)
+console.log(typeof convertedScore)
+console.log(convertedScore)
+
+let check = "suman"
+let checker = Boolean(check);
+// 1-> true ; 0-> false
+// "" ->false 
+//"suman" -> true
+
