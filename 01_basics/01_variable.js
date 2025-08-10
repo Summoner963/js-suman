0dd78a1fb82cc740ea2e4a36e29612f90a7217e7
Summoner963 <theonesummonerzzz@gmail.com>
2025-08-10 10:24:48 +0000

    variables and datatyes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,17 @@
+const accountId = 12345
+let accEmaiil = "hello@gmail.com"
+var accPassword = "12345678"
+accCity = "ktm"
+
+// accountId = 2 // not allowed
+accEmaiil ="hi@gmail.com"
+accPassword ="8776543"
+accCity = "bkt"
+/*
+prefer not use var due to
+block and functional scope issue 
+*/
+
+ 
+
+console.table([accountId,accEmaiil,accPassword,accCity]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";// treat all js code as newer version
+
+// alert("hi man"); in node js not usable right now .but used in browser
+let name="suman"
+let age = 21
+let isLoggedIn= false
+let undefEg;
+let nullEg =null;
+
+// primitive >>>>>
+// number +> 2 to power 53
+// bigINt
+// string =>""
+// boolean=?true/false
+//null => standalone value
+//undefined =>
+    // symbol =>
+
+// non primitive 
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
+
+
